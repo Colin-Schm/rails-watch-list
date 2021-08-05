7395021d90a3e063d31c3e74e190652befdef493
colin schmidt <colin.schm@gmail.com>
2021-08-05 16:29:36 +0200

    Exo 2.3 bookmark links

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,32 @@
 class BookmarksController < ApplicationController
+
+	before_action :get_book_params, only: [:show]
+
+	def index
+		@bookmarks = Bookmark.all
+	end
+
+	def show
+	end
+
+	def new
+    @list = List.find(params[:list_id])
+		@bookmark = Bookmark.new	
+	end
+
+	def create
+		@bookmark = Bookmark.new(bookmark_params)
+		@bookmark.save
+		redirect_to lists_path(@list)
+	end
+
+	def get_book_params
+		@list = Bookmark.find(params[:id])
+	end
+
+	private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,31 @@
 class ListsController < ApplicationController
+
+	before_action :get_params, only: [:show]
+
+	def index
+		@lists = List.all
+	end
+
+	def show
+	end
+
+	def new
+		@list = List.new
+	end
+
+	def create
+		@list = List.new(list_params)
+		@list.save
+		redirect_to lists_path(@list)
+	end
+
+	def get_params
+		@list = List.find(params[:id])
+	end
+
+	private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
